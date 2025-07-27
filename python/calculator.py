cca7714ac832ca5caf3b2dd344ec70e20445291d
divinepatrick <patrickdivine70@gmail.com>
2025-07-27 17:03:30 +0100

    initial commit

---
+++ python/calculator.py
@@ -0,0 +1,8 @@
+def addition_calculator():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+
+
+addition_calculator()
